12695b395499251f21d55e52080971bfdd90fa0b
Cameron Wong <hgneng@gmail.com>
2023-02-13 09:20:22 +0800

    add link

--- init.sh
+++ init.sh
@@ -2,4 +2,7 @@
 sudo apt install libcudart11.0
 mkdir ~/.pip
 cp pip.conf ~/.pip/
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+mkdir -p /data/speech_data/
+ln -s /gemini/data-1 /data/speech_data/data_thchs30
